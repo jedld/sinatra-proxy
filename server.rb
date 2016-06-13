d04660064d1b167ff34e1194abfa639f5b2510e7
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2016-06-13 20:11:03 +0800

    Fix typo

--- server.rb
+++ server.rb
@@ -15,7 +15,7 @@ $config['mapping'].each do |mapping|
   send(mapping['method'].to_sym, "#{mapping['path']}/*") do
     path = "#{mapping['host']}/#{request.path.gsub(mapping['path'],'')}"
     #build http party url
-    response = if (mapping['medthod'].downcase == 'get')
+    response = if mapping['method'].downcase == 'get'
       HTTParty.get(path, query: params, headers: headers)
     else
       HTTParty.post(path, body: request.body, headers: headers)
