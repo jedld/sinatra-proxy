664f50c0cc14d50d09044dc5f834a0c5ff9dca98
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2016-06-14 16:48:11 +0800

    Add echo endpoint

--- server.rb
+++ server.rb
@@ -2,11 +2,31 @@ require 'sinatra'
 require 'open-uri'
 require 'yaml'
 require 'httparty'
+require 'json'
 
 $config = YAML.load_file('config.yml')
 
 get "/" do
   "OK"
+end
+
+get "/echo/*" do
+  mapped_headers = {}
+  headers.each do |k,v|
+    mapped_headers[k] = v if v
+  end
+  content_type 'application/json'
+  { headers: mapped_headers, params: params, path: request.path }.to_json
+end
+
+post "/echo/*" do
+  mapped_headers = {}
+  headers.each do |k,v|
+    mapped_headers[k] = v if v
+  end
+  
+  content_type 'application/json'
+  { headers: mapped_headers, body: request.body, params: params, path: request.path }.to_json
 end
 
 
